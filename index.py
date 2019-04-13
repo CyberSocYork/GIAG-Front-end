e4ebcaad96b30f7b8c9a1489701d40146d006b48
Asim Poptani <asim@poptani.com>
2019-04-13 22:16:59 +0100

    Try that
    Signed-off-by: Asim Poptani <asim@poptani.com>

--- index.py
+++ index.py
@@ -12,8 +12,10 @@ def home():
             for danger in dangerous:
                 if (danger in command):
                       return render_template("home.html", output="We caught you doing something stupid and dangerous! <br/> Don't do it again or else ...")
-            output=subprocess.check_output(command.split() ,  stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=0.01)
-            output=output.stdout.decode('utf-8')
+            print(command.split())
+            output=subprocess.check_output(command.split(), timeout=0.01)
+            print(output)
+            output=output.decode('utf-8')
             output=output.split("\n")[::-1]
             final=""
             for a in output:
